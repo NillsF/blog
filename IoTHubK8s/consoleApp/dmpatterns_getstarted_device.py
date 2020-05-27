b7c7de7d293996fbaac0bfe1247ebe4aaeb46c00
Nills Franssens <nills.franssens@gmail.com>
2020-05-27 14:55:20 -0700

    Adding IoT example

---
+++ IoTHubK8s/consoleApp/dmpatterns_getstarted_device.py
@@ -0,0 +1,40 @@
+import time
+import json
+import threading
+from azure.iot.device import IoTHubModuleClient
+
+with open('/var/secrets/device_iot_conn_string', 'r') as f:
+    CONNECTION_STRING = f.readline()
+
+# uncomment line below if you want to see the connection string.
+# print(CONNECTION_STRING)
+
+def twin_update_listener(client):
+    while True:
+        patch = client.receive_twin_desired_properties_patch()  # blocking call
+        print("")
+        print("Twin desired properties patch received:")
+        print(patch)
+        print("Now is the time to execute business logic.")
+
+
+def iothub_client_sample_run():
+    try:
+        module_client = IoTHubModuleClient.create_from_connection_string(CONNECTION_STRING)
+
+        twin_update_listener_thread = threading.Thread(target=twin_update_listener, args=(module_client,))
+        twin_update_listener_thread.daemon = True
+        twin_update_listener_thread.start()
+
+        while True:
+            time.sleep(1000000)
+
+    except KeyboardInterrupt:
+        print("IoTHubModuleClient sample stopped")
+
+
+if __name__ == '__main__':
+    print ( "Starting the IoT Hub Python sample..." )
+    print ( "IoTHubModuleClient waiting for commands, press Ctrl-C to exit" )
+
+    iothub_client_sample_run()
