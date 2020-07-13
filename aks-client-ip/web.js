f6041eb6092761fdcb2aeb20dbaeae0aca567954
Nills Franssens <nills.franssens@gmail.com>
2020-07-13 15:55:09 -0700

    nodejs IP created

---
+++ aks-client-ip/web.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write(req.connection.remoteAddress); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
