dd426c5a2ef2ff2d05e4e235e8ac8643cbbe5947
Nills Franssens <nills.franssens@gmail.com>
2020-09-28 18:03:42 -0700

    python disk change

---
+++ change-disks/pip-install.sh
@@ -0,0 +1,11 @@
+# Add the Resource Graph library for Python
+pip3 install azure-mgmt-resourcegraph
+
+# Add the Resources library for Python
+pip3 install azure-mgmt-resource
+
+# Add the CLI Core library for Python for authentication (development only!)
+pip3 install azure-cli-core
+
+# Add compute library
+pip3 install azure-mgmt-compute
