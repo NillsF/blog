8284d607f5c18a50603cddfcde2847b0ec9463b6
Nills Franssens <nilfran@microsoft.com>
2021-04-02 14:07:33 -0700

    quick pass through shared sig

--- image-gallery-cross-tenant-az-cli/script.sh
+++ image-gallery-cross-tenant-az-cli/script.sh
@@ -1,11 +1,12 @@
 RGNAME="sig-cross-tenant"
 LOC="WESTUS2"
 GALLERY="nfGallery"
-VMNAME="sigsource"
+VMNAME="sigsource" #used to create an image from
 IMAGENAME="sigtestimage"
 TENANT1="72f988bf-86f1-41af-91ab-2d7cd011db47" #MSFT
-TENANT2="42949aa4-09db-4624-9a32-83e2e02758c5" #VS
+TENANT2="42949aa4-09db-4624-9a32-83e2e02758c5" #MSDN
 TARGETSUBID="ea122fb3-39a8-411c-995c-3724e344095f"
+
 ##### 
 # All of this executed in primary tenant
 #####
@@ -14,8 +15,8 @@ echo "Creating RG and VM"
 az group create -n $RGNAME -l $LOC
 
 # Create a VM to create an image from
-az vm create -n $VMNAME -g $RGNAME --admin-username nilfranadmin \
-    --ssh-key-values ~/.ssh/id_rsa.pub --image ubuntults
+vmip=$(az vm create -n $VMNAME -g $RGNAME --admin-username nilfranadmin \
+    --ssh-key-values ~/.ssh/id_rsa.pub --image ubuntults --query publicIpAddress -o tsv)
 
 # Sleep 30 to make sure VM is available to run command
 echo "Sleeping 30 seconds to wait for VM to be fully ready"
@@ -23,17 +24,14 @@ sleep 30
 
 # Generalize VM
 echo "Generalizing VM"
+ssh -o StrictHostKeyChecking=no nilfranadmin@$vmip 'sudo waagent -deprovision+user -force'
+echo "sleeping to ensure deprovision succeeded"
+sleep 10
 
-az vm run-command invoke -g $RGNAME -n $VMNAME --command-id RunShellScript --scripts "sudo waagent -deprovision+user -force"
 az vm deallocate -g $RGNAME -n $VMNAME
 az vm generalize -g $RGNAME -n $VMNAME
 
-# Capture image
-echo "Creating image"
-az image create -g $RGNAME -n $IMAGENAME --source $VMNAME
-
-# Get image id
-imageid=$(az image show -g $RGNAME -n $IMAGENAME -o tsv --query id)
+vmid=$(az vm show -g $RGNAME -n $VMNAME -o tsv --query id)
 
 # Create image gallery 
 echo "Creating sig"
@@ -42,7 +40,7 @@ sigid=$(az sig show \
    --resource-group $RGNAME \
    --gallery-name $GALLERY \
    --query id -o tsv)
-echo "importing image into sig"
+echo "Creating image in sig"
 az sig image-definition create \
    --resource-group $RGNAME \
    --gallery-name $GALLERY \
@@ -61,11 +59,11 @@ az sig image-version create \
    --gallery-image-version 1.0.0 \
    --target-regions $LOC \
    --replica-count 1 \
-   --managed-image $imageid
+   --managed-image $vmid
 
 # Get image ID from sig
 sigimageid=$(az sig image-version show --gallery-image-definition $GALLERY \
-                          --gallery-image-version 1.0.1 \
+                          --gallery-image-version 1.0.0 \
                           --gallery-name $GALLERY \
                           --resource-group $RGNAME \
                           --query id -o tsv)
@@ -92,12 +90,13 @@ az role assignment create --assignee $appid --role "Reader" --scope $sigid
 ####
 # Now login to the other tenant
 ####
+az account clear
 az login
 
 # Now create an SP in this tenant, using the same app
 az ad sp create --id $appid
 
-# Give this SP permissions to create stuff
+# Give this SP permissions to create VMs
 az role assignment create --assignee $appid --role "Contributor"
 ### 
 # To create VM, login using the SP to both tenants now.
@@ -106,9 +105,7 @@ az role assignment create --assignee $appid --role "Contributor"
 
 az account clear
 az login --service-principal -u $appid -p $pw --tenant $TENANT1
-az account get-access-token 
 az login --service-principal -u $appid -p $pw --tenant $TENANT2
-az account get-access-token
 
 # Create RG in target
 az group create -n $RGNAME -l $LOC --subscription $TARGETSUBID
@@ -118,4 +115,5 @@ az vm create \
   -n $VMNAME \
   --image $sigimageid \
   --admin-username nilfranadmin \
-  --ssh-key-values ~/.ssh/id_rsa.pub
+  --ssh-key-values ~/.ssh/id_rsa.pub \
+  --subscription $TARGETSUBID
